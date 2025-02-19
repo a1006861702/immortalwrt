09aa90320b089f6ae45b31a56f22faf720e4d75d
a1006861702 <48322501+a1006861702@users.noreply.github.com>
2025-02-19 14:24:30 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,5 +1,5 @@
 # 6-添加 lucky 插件
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/a1006861702/luci-app-lucky.git package/lucky
 
 # 7-添加 wizard 插件
-git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
+# git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
